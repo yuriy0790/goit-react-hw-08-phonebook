1bd67cc9c46419cc9dbf9c529f66e42e2d2c12ab
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-27 16:59:38 +0200

    feat: redirect routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,21 +8,48 @@ import ContactsPage from 'pages/ContactsPage/ContactsPage';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks';
+import Loader from './Loader/Loader';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  distance: '60px',
+});
 
 const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
